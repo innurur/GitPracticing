170774163bc7411c93eec6831f62a97dc5a91969
Inara <inarasalih2015@gmail.com>
2021-01-28 23:07:13 -0600

    TEstCase1 is created

---
+++ src/NewProjectNameHere/TestCase1.java
@@ -0,0 +1,12 @@
+package NewProjectNameHere;
+
+public class TestCase1 {
+    public static void main(String[] args) {
+     // here is a  lot of codes
+     /*
+     First we have to create an object
+     then we have to go to VCS and enable the version control
+
+      */
+    }
+}
