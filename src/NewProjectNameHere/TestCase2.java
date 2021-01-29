c2485c7ce1e41a4daeca6eccf74975eb6079ed5c
Inara <inarasalih2015@gmail.com>
2021-01-28 23:23:19 -0600

    TestCase3 is created

--- src/NewProjectNameHere/TestCase2.java
+++ src/NewProjectNameHere/TestCase2.java
@@ -7,6 +7,10 @@ public class TestCase2 {
         cause I still dont know the point
         why and where should we use it
 
+also I personally should practice to clone the project
+because everytime I was creating it
+and I have to practice collaboration
+but the other way
          */
     }
 }
