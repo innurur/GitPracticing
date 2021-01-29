a76cd414c3bbcc35ae85f61e018b44501992b55b
Inara <inarasalih2015@gmail.com>
2021-01-28 23:18:32 -0600

    TestCase2 is created

---
+++ src/NewProjectNameHere/TestCase2.java
@@ -0,0 +1,12 @@
+package NewProjectNameHere;
+
+public class TestCase2 {
+    public static void main(String[] args) {
+        /*
+        here we will practice some branches
+        cause I still dont know the point
+        why and where should we use it
+
+         */
+    }
+}
