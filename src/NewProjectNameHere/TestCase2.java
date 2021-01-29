3d66f4e92ddf300ca8121f25d64ff521201eefb8
Inara <inarasalih2015@gmail.com>
2021-01-28 23:20:39 -0600

    TestCase2 has some changes

--- src/NewProjectNameHere/TestCase2.java
+++ src/NewProjectNameHere/TestCase2.java
@@ -7,6 +7,13 @@ public class TestCase2 {
         cause I still dont know the point
         why and where should we use it
 
+    now im working in a different branch and
+    will see the difference between
+    checking out  and merging strict and
+    checking out without merging
+    but using the VCS merge
+
+
          */
     }
 }
